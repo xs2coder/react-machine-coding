8c5b07a4934460d0483fa6295ee9f954ed3bb402
gs <gs@kulvirs-MacBook-Air.local>
2025-05-21 18:40:26 -0700

    project setup using Vite and Tailwind

---
+++ README.md
@@ -0,0 +1,1 @@
+# React Machine Coding Challenges
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+export const App = () => {
+    return (
+        <div className="text-3xl font-bold bg-blue-200 underline">App</div>
+    )
+}
+export default App;
